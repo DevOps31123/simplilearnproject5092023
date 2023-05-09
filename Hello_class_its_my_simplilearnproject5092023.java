220bb0777d58d42ce63adcf4713ed325a71464b3
yevheniy <yevheniy2005@gmail.com>
2023-05-09 18:59:01 +0000

    Dockerfiles created for new images

---
+++ Hello_class_its_my_simplilearnproject5092023.java
@@ -0,0 +1,5 @@
+public class Hello_class_its_my_simplilearnproject5092023 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Hello_class_its_my_simplilearnproject5092023!");
